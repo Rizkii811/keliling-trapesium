9444995cb69f0f867555aeabc6ecbae5110f4017
Rizkii811 <rizkiefendi646@gmail.com>
2025-02-11 21:36:16 +0700

    Add files via upload

---
+++ trapesium.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int a, b, c, d;
+	float k;
+	cout<<"panjang pertama";
+	cin>>a;
+	cout<<"panjang kedua";
+	cin>>b;
+	cout<<"panjang ketiga";
+	cin>>c;
+	cout<<"panjang keempat";
+	cin>>d;
+	k= a+b+c+d;
+	cout<<"keliling= "<<k;
+	return 0;
+	}
